746ad482b6c5846801829730f90c2ee355ec611e
Gabriel Erion <erion@cs.washington.edu>
2021-03-30 13:25:36 -0700

    Add Torch GLRT on coefs

--- uncertainty/glrtTorch.py
+++ uncertainty/glrtTorch.py
@@ -13,18 +13,23 @@ def highCoefObj(idx,lmbd,llh,global_attributions):
 def MSE(y, yPred):
     return np.mean((y - yPred)**2)
 
-def glrtTorchCis(modelFn,X,y,alpha=0.05,bootstrap_kwargs={},search_kwargs={},fit_kwargs={}):
+def glrtTorchCis(modelFn,X,y,alpha=0.05,citype='attribs',bootstrap_kwargs={},search_kwargs={},fit_kwargs={}):
     lcb_LR, ucb_LR = bootstrapGLRTcis(modelFn, X, y, MSE, alpha=alpha, **bootstrap_kwargs)
     lcbs, ucbs = [], []
     lcb_all_results, ucb_all_results = [], []
     for idx in range(X.shape[1]):
-        ucb_Coef, ucb_Results = getBoundary(modelFn,X,y,idx,ucb=ucb_LR,obj=highCoefObj,reduction=np.max,fit_kwargs=fit_kwargs,**search_kwargs)
-        lcb_Coef, lcb_Results = getBoundary(modelFn,X,y,idx,ucb=ucb_LR,obj=lowCoefObj,reduction=np.min,fit_kwargs=fit_kwargs,**search_kwargs)
+        if citype=='attribs':
+            ucb_Coef, ucb_Results = getBoundary(modelFn,X,y,idx,ucb=ucb_LR,obj=highCoefObj,reduction=np.max,fit_kwargs=fit_kwargs,**search_kwargs)
+            lcb_Coef, lcb_Results = getBoundary(modelFn,X,y,idx,ucb=ucb_LR,obj=lowCoefObj,reduction=np.min,fit_kwargs=fit_kwargs,**search_kwargs)
+        elif citype=='coefs':
+            ucb_Coef, ucb_Results = getBoundaryCoef(modelFn,X,y,idx,ucb=ucb_LR,obj=highCoefObj,reduction=np.max,fit_kwargs=fit_kwargs,**search_kwargs)
+            lcb_Coef, lcb_Results = getBoundaryCoef(modelFn,X,y,idx,ucb=ucb_LR,obj=lowCoefObj,reduction=np.min,fit_kwargs=fit_kwargs,**search_kwargs)
+        else: raise ValueError('Only attribs and coefs are supported citypes!')
         lcbs.append(lcb_Coef)
         ucbs.append(ucb_Coef)
         lcb_all_results.append(lcb_Results)
         ucb_all_results.append(ucb_Results)
-    return np.array(lcbs), np.array(ucbs), lcb_all_results, ucb_all_results
+    return np.array(lcbs), np.array(ucbs), lcb_all_results, ucb_all_results, lcb_LR, ucb_LR
 
 def getBoundary(modelFn,X,y,idx,ucb,obj=lowCoefObj,reduction=np.min,lmbds=np.logspace(-10,10,101),lossfunc=torch.nn.functional.mse_loss,fit_kwargs={}):
     Xtorch = torch.Tensor(X)
@@ -59,6 +64,44 @@ def trainWithAttributions(model,X,y,obj,lossfunc,lr=0.001,max_iter=1000):
             preds, attribs = model(X,shap_values=True)
             mse = lossfunc(preds.flatten(),y)
             global_attribs = attribs.abs().mean(0)
+            total_obj = obj(mse,global_attribs)
+            
+            train_scores.append((total_obj.item(),mse.item()))
+            total_obj.backward()
+            opt.step()
+
+def getBoundaryCoef(modelFn,X,y,idx,ucb,obj=lowCoefObj,reduction=np.min,lmbds=np.logspace(-10,10,101),lossfunc=torch.nn.functional.mse_loss,fit_kwargs={}):
+    Xtorch = torch.Tensor(X)
+    ytorch = torch.Tensor(y)
+    Rtorch = torch.ones_like(Xtorch)*Xtorch.mean(0).reshape(1,-1)
+    mses, coefs, biases, attributions = [], [], [], []
+    for lmbd in lmbds:
+        WrapperModel = modelFn()
+        TorchModel = WrapperModel._init_model(X.shape[1])
+        TorchModel.coefs = WrapperModel.linear_layer.weight.flatten()
+        idxObj = partial(obj,idx,lmbd)
+        trainWithCoefs(TorchModel,Xtorch,ytorch,idxObj,lossfunc,**fit_kwargs)
+        preds = TorchModel(Xtorch)
+        mse = lossfunc(ytorch,preds.flatten())
+        coef = WrapperModel.linear_layer.weight.detach().numpy().flatten()
+        bias = WrapperModel.linear_layer.bias.detach().item()
+        attrib = coef[idx]
+        mses.append(mse.item())
+        attributions.append(attrib.item())
+        coefs.append(coef)
+        biases.append(bias)
+        
+    mses, attributions, coefs, biases = np.array(mses), np.array(attributions), np.array(coefs), np.array(biases)
+    return reduction(attributions[mses<=ucb]), (mses, attributions, coefs, biases)
+    
+def trainWithCoefs(model,X,y,obj,lossfunc,lr=0.001,max_iter=1000):
+        train_scores=[]
+        opt = torch.optim.SGD(model.parameters(),lr=lr)
+        for i in range(max_iter):
+            opt.zero_grad()
+            preds = model(X)
+            mse = lossfunc(preds.flatten(),y)
+            global_attribs = model.coefs
             total_obj = obj(mse,global_attribs)
             
             train_scores.append((total_obj.item(),mse.item()))
